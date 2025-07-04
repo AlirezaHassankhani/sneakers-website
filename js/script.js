1d83a4d55e1187d4014a7ff44eb00f2da227b757
flatrov <flatrov1@gmail.com>
2025-07-04 12:57:12 +0330

    complate menu lagic

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
+"use strict"
 
+const $ = document;
+
+let overlayTag = $.querySelector(".overlay");
+
+let navigationOpenBtn = $.querySelector(".navigation-open");
+let navigationClose = $.querySelector(".navigation-close");
+let navigationMenu = $.querySelector(".navigation");
+
+
+const navigationOpenHandler = () => {
+    navigationMenu.classList.replace("-left-64", "left-0");
+    overlayTag.classList.remove(...["invisible", "opacity-0"]);
+}
+
+const navigationCloseHandler = () => {
+    navigationMenu.classList.replace("left-0", "-left-64");
+    overlayTag.classList.add(...["invisible", "opacity-0"]);
+}
+
+navigationOpenBtn.addEventListener("click", navigationOpenHandler);
+navigationClose.addEventListener("click", navigationCloseHandler);
+overlayTag.addEventListener("click", navigationCloseHandler);
