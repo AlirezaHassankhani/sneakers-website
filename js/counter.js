39794dc3875f98059310a5b2a7a69e432eaeba13
flatrov <flatrov1@gmail.com>
2025-07-05 12:19:36 +0330

    update slider box

--- js/counter.js
+++ js/counter.js
@@ -1,21 +1,21 @@
 class Counter {
-  count = 0;
-  constructor(count) {
-    this.count = count;
-  }
-  increaseCount() {
-    this.count += 1;
-  }
-  decreaseCount() {
-    if (this.count > 0) {
-      this.count -= 1;
+    count = 0;
+    constructor(count) {
+        this.count = count;
     }
-  }
-  setCount(input) {
-    this.count = input;
-  }
-  getCount() {
-    return this.count;
-  }
+    increaseCount() {
+        this.count += 1;
+    }
+    decreaseCount() {
+        if (this.count > 0) {
+            this.count -= 1;
+        }
+    }
+    setCount(input) {
+        this.count = input;
+    }
+    getCount() {
+        return this.count;
+    }
 }
 export default Counter;
--- js/slider.js
+++ js/slider.js
@@ -3,35 +3,43 @@ let galleryPicture = document.querySelector(".gallery-picture");
 let overal = document.querySelector(".overlay");
 let slider = document.querySelector(".slider");
 let currentPicture = document.querySelector(".current-picture");
+let sliderNavigation = document.querySelector(".slider-navigation");
 let picture = [
-  {
-    id: "1",
-    src: "./assets/images/image-product-1.jpg",
-  },
-  {
-    id: "2",
-    src: "./assets/images/image-product-2.jpg",
-  },
-  {
-    id: "3",
-    src: "./assets/images/image-product-3.jpg",
-  },
-  {
-    id: "4",
-    src: "./assets/images/image-product-4.jpg",
-  },
+    {
+        id: "1",
+        src: "./assets/images/image-product-1.jpg",
+    },
+    {
+        id: "2",
+        src: "./assets/images/image-product-2.jpg",
+    },
+    {
+        id: "3",
+        src: "./assets/images/image-product-3.jpg",
+    },
+    {
+        id: "4",
+        src: "./assets/images/image-product-4.jpg",
+    },
 ];
-galleryPicture?.querySelectorAll("img").forEach((image) =>
-  image.addEventListener("click", (e) => {
+galleryPicture?.querySelectorAll("div").forEach((div) => div.addEventListener("click", () => {
     overal?.classList.remove(...["invisible", "opacity-0"]);
     slider?.classList.remove(...["invisible", "opacity-0"]);
-    let src = picture.find((i) => i.id == image.dataset.id);
+    let { id, src } = picture.find((i) => i.id == div.dataset.id);
     if (currentPicture) {
-      currentPicture.innerHTML = "";
-      let newImage = document.createElement("img");
-      newImage.classList.add(...["w-md", "rounded-2xl"]);
-      if (src) newImage.src = src.src;
-      currentPicture.appendChild(newImage);
+        currentPicture.dataset.id = id;
+        currentPicture.src = src;
     }
-  })
-);
+}));
+sliderNavigation?.querySelectorAll("div").forEach((div) => {
+    div.addEventListener("click", () => {
+        let previousImg = sliderNavigation?.querySelector(`[data-id="${currentPicture.dataset.id}"]`);
+        previousImg.dataset.isSelected = "false";
+        div.dataset.isSelected = "true";
+        let { id, src } = picture.find((i) => i.id == div.dataset.id);
+        if (currentPicture) {
+            currentPicture.dataset.id = id;
+            currentPicture.src = src;
+        }
+    });
+});
