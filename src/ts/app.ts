720684fe5f80f9f63a2873211be9d2d379b167f0
flatrov <flatrov1@gmail.com>
2025-07-24 13:07:47 +0330

    add global event function

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -6,6 +6,23 @@ const mobileMenu = $.querySelector(".mobile-menu");
 const menuOpenBtn = $.querySelector(".menu-open-btn");
 const menuCloseBtn = $.querySelector(".menu-close-btn");
 
+const cartBox = $.querySelector(".cart-box");
+
+// Global event
+function globalEvent(
+  selector: string,
+  type: string,
+  parent: Element | null,
+  callback: (e: Event) => void
+) {
+  parent?.addEventListener(type, function (e) {
+    const target = e.target;
+    if (target instanceof HTMLElement) {
+      if (target.closest(selector)) callback(e);
+    }
+  });
+}
+
 // Overlay logic
 overlay?.addEventListener("click", closeOverlay);
 
@@ -34,15 +51,17 @@ menuOpenBtn?.addEventListener("click", openMenuHandler);
 menuCloseBtn?.addEventListener("click", closeMenuHandler);
 
 function openMenuHandler() {
-    openOverlay();
-    if(mobileMenu instanceof HTMLElement) {
-        mobileMenu.dataset.isOpen = "true";
-    }
+  openOverlay();
+  if (mobileMenu instanceof HTMLElement) {
+    mobileMenu.dataset.isOpen = "true";
+  }
 }
 
 function closeMenuHandler() {
-    closeOverlay();
-    if(mobileMenu instanceof HTMLElement) {
-        mobileMenu.dataset.isOpen = "false";
-    }
+  closeOverlay();
+  if (mobileMenu instanceof HTMLElement) {
+    mobileMenu.dataset.isOpen = "false";
+  }
 }
+
+
